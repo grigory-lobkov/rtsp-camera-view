e095ad1c6011934bca053122623f384f15ff05ed
Grigory Lobkov <gg81@ya.ru>
2018-07-23 21:25:30 +0500

    1.0.1

--- Presenter/Properties/AssemblyInfo.cs
+++ Presenter/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Rtsp Camera View presenter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MVP Presenter")]
+[assembly: AssemblyDescription("https://github.com/grigory-lobkov/rtsp-camera-view")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rtsp Camera View presenter")]
+[assembly: AssemblyProduct("RTSP Camera View")]
 [assembly: AssemblyCopyright("Copyright © 2018 Grigory Lobkov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
--- View/Properties/AssemblyInfo.cs
+++ View/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("https://github.com/grigory-lobkov/rtsp-camera-view")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RTSP mosaic VLC player")]
+[assembly: AssemblyProduct("RTSP Camera View")]
 [assembly: AssemblyCopyright("Copyright © 2018 Grigory Lobkov")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.6")]
-[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
